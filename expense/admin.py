104cd215d92a2bd31657911c565658b61c2c31ae
Emmanuel B <emmanuel.ballerini@gmail.com>
2024-04-05 07:47:13 -0400

    add search field to Transaction in admin

--- expense/admin.py
+++ expense/admin.py
@@ -4,8 +4,12 @@ from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 from .models import Account, Category, CreditCard, Transaction, User
 
+class TransactionAdmin(admin.ModelAdmin):
+    search_fields = ["description"]
+
+
 admin.site.register(Account)
 admin.site.register(CreditCard)
 admin.site.register(Category)
-admin.site.register(Transaction)
+admin.site.register(Transaction, TransactionAdmin)
 admin.site.register(User, UserAdmin)
